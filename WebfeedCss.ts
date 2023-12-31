287ed38734cc7ef38f7fa3aaca73ae13f4033f96
paulgo <spam@heropaul.com>
2023-12-31 10:29:59 -0500

    Initial

---
+++ WebfeedCss.ts
@@ -0,0 +1,26 @@
+
+const webfeedCss: (string | Raw.Param) = [
+	"HTML", {
+		scrollSnapType: "y mandatory",
+	},
+	"HTML, BODY", {
+		margin: 0,
+		padding: 0,
+		height: "100%",
+	},
+	"HTML", {
+		overflowY: "auto",
+	},
+	"SECTION", {
+		position: "relative",
+		overflow: "hidden",
+		height: "100%",
+		paddingTop: "0.02px",
+		paddingBottom: "0.02px",
+		scrollSnapAlign: "start",
+		scrollSnapStop: "always",
+	}
+];
+
+//@ts-ignore
+typeof module !== "undefined" && (module.exports = webfeedCss);
---
+++ readme.md
@@ -0,0 +1,15 @@
+
+# Webfeed CSS
+
+This repository contains both a CSS as well as a TypeScript version of the minimum CSS required to render a Webfeed in a browser environment. The TypeScript version has RawJS typings, so the CSS can be an argument in a call to `raw.style()` or `raw.css()`.
+
+The CSS file is available on 
+
+## CommonJS Installation / Usage
+
+```
+npm install webfeed-css
+```
+```
+const webfeedCss = require("
+```
